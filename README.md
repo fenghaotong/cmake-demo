30c9eb0917e2f4a65b551a9f4cf7bbd2a652c14c
fenghaotong <fenghaotong@datagrand.com>
2021-08-06 10:51:25 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## CMAKE demo
+
+#### 编译
+
+```
+# 根目录
+cmake -A [Win32 | Win64] -B build
+
+cmake --build build --config [Release | Debug]
+
+# 创建文件夹
+mkdir build
+cd build
+cmake .. 
+```
+
+> cmake详细命令可`cmake --help`查看
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <Windows.h>
+#include <iostream>
+
+typedef int (*add)(int, int); 
+
+int main() {
+  HINSTANCE handle = LoadLibrary("my_dll.dll"); 
+  
+  std::cout << handle << std::endl; 
+  auto f = (add) GetProcAddress(handle, "add"); 
+  std::cout << f(1, 32) << std::endl; 
+  FreeLibrary(handle);
+  std::cout << "hello world" << std::endl;
+  return 0; 
+}
